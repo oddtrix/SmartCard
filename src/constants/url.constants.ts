47bb49c36edfbeaf074263ef68314ed44488c5bb
odd <flinqwe@gmail.com>
2023-11-01 22:38:05 +0200

    env fix

--- src/constants/url.constants.ts
+++ src/constants/url.constants.ts
@@ -1,3 +1,3 @@
 // export const baseUrl = "http://localhost:13772/api";
 // export const baseUrl = "http://localhost:8090/api";
-export const baseUrl = process.env.apiURL;
+export const baseUrl = process.env.WEB_API_URL;
