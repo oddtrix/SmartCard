2116160ba96861d869d7d0cb09df46c5050478ab
odd <flinqwe@gmail.com>
2023-11-01 22:15:52 +0200

    added env api

--- src/constants/url.constants.ts
+++ src/constants/url.constants.ts
@@ -1,2 +1,3 @@
 // export const baseUrl = "http://localhost:13772/api";
-export const baseUrl = "http://localhost:8090/api";
+// export const baseUrl = "http://localhost:8090/api";
+export const baseUrl = process.env.apiURL;
