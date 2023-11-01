700b84c39fc2020627b6ddb212382ed550d5af1a
odd <flinqwe@gmail.com>
2023-11-01 22:44:16 +0200

    env fix

--- src/constants/url.constants.ts
+++ src/constants/url.constants.ts
@@ -1,3 +1,3 @@
 // export const baseUrl = "http://localhost:13772/api";
 // export const baseUrl = "http://localhost:8090/api";
-export const baseUrl = process.env.WEB_API_URL;
+export const baseUrl = "https://smartcardapi.azurewebsites.net/";
