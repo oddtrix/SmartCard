d345a190c8fea455317d7b7b7c1eb075cba20d23
vladyslav.ilnytskyi <vladyslav.ilnytskyi@nure.ua>
2024-04-12 13:22:06 +0300

    server url changed

--- src/constants/url.constants.ts
+++ src/constants/url.constants.ts
@@ -1,3 +1,3 @@
-// export const baseUrl = "http://localhost:13772/";
+export const baseUrl = "http://localhost:13772/";
 // export const baseUrl = "http://localhost:8090/api";
-export const baseUrl = "https://smartcardapi.azurewebsites.net/";
+// export const baseUrl = "https://smartcardapi.azurewebsites.net/";
